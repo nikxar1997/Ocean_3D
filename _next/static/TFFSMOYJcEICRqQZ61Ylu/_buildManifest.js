df5e61cd1b318c15256ce06590fcc4fc39f956a0
Nikolaos Charanas <nikos.charanas@prismael.com>
2026-05-19 16:39:28 +0300

    Updates

--- _next/static/TFFSMOYJcEICRqQZ61Ylu/_buildManifest.js
+++ _next/static/TFFSMOYJcEICRqQZ61Ylu/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Ocean_3D//_next/:path+",
+        "destination": "/Ocean_3D/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
